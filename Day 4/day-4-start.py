0ef8e1007fbfaa881e8728ecdd038d5dcf773b59
Anne Goode <thegoodecode@gmail.com>
2024-02-21 12:44:08 +0100

    day-4-start-1

--- Day 4/day-4-start.py
+++ Day 4/day-4-start.py
@@ -9,3 +9,14 @@ print(random_float)
 
 love_score = random.randint(1, 100)
 print(f"Your love score is {love_score}")
+
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia"] 
+
+states_of_america[1] = "Penniesalvania"
+
+print(states_of_america)
+
+states_of_america.append("Anneland")
+
+print(states_of_america)
