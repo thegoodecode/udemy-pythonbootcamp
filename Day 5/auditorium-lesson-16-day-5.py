43c315da1c7a8bfa3a168a518b041dd2015bedb6
Anne Goode <thegoodecode@gmail.com>
2024-02-22 10:30:29 +0100

    indentation check

--- Day 5/auditorium-lesson-16-day-5.py
+++ Day 5/auditorium-lesson-16-day-5.py
@@ -1,10 +1,10 @@
-# Input a Python list of student heights
+  # Input a Python list of student heights
 student_heights = input().split()
 for n in range(0, len(student_heights)):
-  student_heights[n] = int(student_heights[n])
-# 🚨 Don't change the code above 👆
-  
-# Write your code below this row 👇
+    student_heights[n] = int(student_heights[n])
+  # 🚨 Don't change the code above 👆
+
+  # Write your code below this row 👇
 
 total_height = 0
 for height in student_heights:
